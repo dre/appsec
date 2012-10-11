c101a1628ad65940047302cc556a978f5c9a7904
Andres Andreu <andres@neurofuzz.com>
2012-10-11 09:52:47 -0400

    structure for variable declaration, somewhat independent of code

---
+++ vars/vhost_finder_vars.py
@@ -0,0 +1,47 @@
+"""
+    variables to be used by the VHostFinder class
+"""
+# modifiable variables
+########################################################
+target_ip = "127.0.0.1"
+target_port = 8080
+target_domain = "domain"
+target_tld = "com"
+useslowdos = False
+anonimize = True
+vhost_finder_threads = 6
+vhost_finder_debug = False
+displayThreshold = 500
+depthLowerBound = 1
+depthUpperBound = 1
+########################################################
+
+def getTargetIp():
+    return target_ip
+
+def getTargetPort():
+    return target_port
+
+def getTargetDomain():
+    return target_domain
+
+def getTargetTld():
+    return target_tld
+
+def getUseSlowDoS():
+    return useslowdos
+
+def getVHostNumThreads():
+    return vhost_finder_threads
+
+def getVHostDebug():
+    return vhost_finder_debug
+
+def getAnonimize():
+    return anonimize
+
+def getDisplayThreshold():
+    return displayThreshold
+
+def getDepthBounds():
+    return(depthLowerBound,depthUpperBound)
