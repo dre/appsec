b4fac90ca20d7053ed018f81d8d02723e2273834
Andres Andreu <andres@neurofuzz.com>
2012-10-12 09:55:28 -0400

    initial ingest

---
+++ DoS/distributed_slow_post/main.py
@@ -0,0 +1,46 @@
+"""
+    Author: Andres Andreu
+    Company: neuroFuzz, LLC
+    Date: 10/12/2012
+    Main controller prog that sets PYTHONPATH and kicks off
+    the entire process.
+
+    MIT-LICENSE
+    Copyright (c) 2012 Andres Andreu, neuroFuzz LLC
+    
+    Permission is hereby granted, free of charge, to any person obtaining a
+    copy of this software and associated documentation files (the "Software"),
+    to deal in the Software without restriction, including without limitation
+    the rights to use, copy, modify, merge, publish, distribute, sublicense,
+    and/or sell copies of the Software, and to permit persons to whom the
+    Software is furnished to do so, subject to the following conditions:
+    
+    The above copyright notice and this permission notice shall be included
+    in all copies or substantial portions of the Software.
+    
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+    EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+    OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+    NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+    HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+    WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+    DEALINGS IN THE SOFTWARE.
+    
+    If you use this for criminal purposes and get caught you are on
+    your own and I am not liable. I wrote this for legit pen testing
+    purposes.
+    
+    Be kewl and give credit where it is due if you use this. Also,
+    send me feedback as I don't have the bandwidth to test for every
+    condition.   
+"""
+import subprocess
+
+proc = subprocess.Popen(['python', 'Distributed_Slow_Post_DDoS.py'],
+                        env={'PYTHONPATH':'../../'}
+                        )
+
+out,err=proc.communicate()
+print(out)
+print(err)
